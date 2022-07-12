9630b655e43f09fc3fa5438133f4b4caf6978962
pmelga01 <79951993+pmelga01@users.noreply.github.com>
2022-07-12 18:34:37 -0400

    Avatar Creator Layout Prototype
    BUG -- arrow images are off centered from button raycast?

--- FV-InternGame/Assets/Level/Scenes/DevTestsPam/OutfitChanger.cs
+++ FV-InternGame/Assets/Level/Scenes/DevTestsPam/OutfitChanger.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OutfitChanger : MonoBehaviour
 {
     //References the sprite to change
     public SpriteRenderer bodyPart;
+    public Image prev;
+    public Image next;
     
     //Sprites to cycle through
     public List<Sprite> options = new List<Sprite>();
@@ -14,7 +17,7 @@ public class OutfitChanger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        changeCarousel();
     }
 
     // Update is called once per frame
@@ -27,11 +30,13 @@ public class OutfitChanger : MonoBehaviour
     {
         currOption++;
         
+        
         if (currOption >= options.Count) {
             currOption = 0;
-        }
+        } 
         
         bodyPart.sprite = options[currOption];
+        changeCarousel();
     }
     
     public void PreviousOption()
@@ -43,11 +48,35 @@ public class OutfitChanger : MonoBehaviour
         }
         
         bodyPart.sprite = options[currOption];
+        changeCarousel();
     }
     
     public void Randomize()
     {
         currOption = Random.Range(0, options.Count - 1);
         bodyPart.sprite = options[currOption];
+        changeCarousel();
+    }
+    
+    void changeCarousel()
+    {
+        if (currOption == 0) {
+            /* If it is the first option, 
+                previous option is last option in list
+                next option is second option in list */
+            prev.sprite = options[options.Count - 1];
+            next.sprite = options[currOption + 1];
+            
+        } else if (currOption == options.Count - 1) {
+            /* If it is the last option, 
+                previous option is last - 1
+                next option is first option */
+            prev.sprite = options[currOption - 1];
+            next.sprite = options[0];
+            
+        } else {
+            prev.sprite = options[currOption - 1];
+            next.sprite = options[currOption + 1];
+        }
     }
 }
