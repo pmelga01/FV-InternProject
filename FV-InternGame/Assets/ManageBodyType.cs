7a3213059b0b8288094faf314c11acecd7546e0d
pmelga01 <79951993+pmelga01@users.noreply.github.com>
2022-08-02 21:01:35 -0400

    Avatar Creation progress

---
+++ FV-InternGame/Assets/ManageBodyType.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageBodyType : MonoBehaviour
+{
+    public GameObject shirtSelection;
+    public GameObject armsSelection;
+    
+    private SpriteRenderer shirt;
+    private SpriteRenderer arms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //shirtSelection
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
