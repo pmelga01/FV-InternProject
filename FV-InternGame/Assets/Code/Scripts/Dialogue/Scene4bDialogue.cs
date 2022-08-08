8863afeca492d0734520f7d539d5df6eb9927800
pmelga01 <79951993+pmelga01@users.noreply.github.com>
2022-08-08 01:04:07 -0700

    lunch scene dialogue (insert ui, 2 constances)

--- FV-InternGame/Assets/Code/Scripts/Dialogue/Scene4bDialogue.cs
+++ FV-InternGame/Assets/Code/Scripts/Dialogue/Scene4bDialogue.cs
@@ -8,6 +8,7 @@ using TMPro;
 
 public class Scene4bDialogue : MonoBehaviour {
         public int primeInt = 1;         // This integer drives game progress!
+        public GameObject mainCharacter;
         public TextMeshProUGUI Char1name;
         public TextMeshProUGUI Char1speech;
         public TextMeshProUGUI Char2name;
@@ -16,6 +17,7 @@ public class Scene4bDialogue : MonoBehaviour {
        //public Text Char3speech;
         public GameObject DialogueDisplay;
         public GameObject ArtChar1;
+        public GameObject ArtChar1_b;
        //public GameObject ArtChar2;
         public GameObject ArtBG1;
         public GameObject Choice1a;
@@ -30,12 +32,14 @@ public class Scene4bDialogue : MonoBehaviour {
 void Start(){         // initial visibility settings
         DialogueDisplay.SetActive(false);
         ArtChar1.SetActive(false);
+        ArtChar1_b.SetActive(false);
         ArtBG1.SetActive(true);
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
         NextScene1Button.SetActive(false);
         NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
+        mainCharacter.SetActive(false);
    }
 
 void Update(){         // use spacebar as Next button
@@ -55,6 +59,7 @@ public void talking()
         }
         else if (primeInt == 2){
                //ArtChar1.SetActive(true);
+               mainCharacter.SetActive(false);
                 DialogueDisplay.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
@@ -68,12 +73,16 @@ public void talking()
             Char2speech.text = "Peanut Butter and Jelly sandwich, Apple, Chips, Burger";
         }
         else if (primeInt == 4) {
+            ArtChar1_b.SetActive(true);
+            mainCharacter.SetActive(false);
             Char1name.text = "CONSTANCE";
             Char1speech.text = "I think I’m just going to have an apple and chips. I feel so fat today.";
             Char2name.text = "";
             Char2speech.text = "";
         }
         else if (primeInt == 5) {
+            ArtChar1_b.SetActive(false);
+            mainCharacter.SetActive(false);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "";
@@ -92,48 +101,57 @@ public void talking()
             Char2speech.text = "Describe the situation";
         }
         else if (primeInt == 8) {
+            mainCharacter.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "YOU";
             Char2speech.text = "You made a comment about weight, and I think you shouldn’t say things like that.";
         }
         else if (primeInt == 9) {
+            mainCharacter.SetActive(false);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = " ~ Express: ~";
             Char2speech.text = "Explain how it made you feel";
         }
         else if (primeInt == 10) {
+            mainCharacter.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "YOU";
             Char2speech.text = "It made me feel uncomfortable to hear you equate a body type with a negative feeling.";
         }
         else if (primeInt == 11) {
+            mainCharacter.SetActive(false);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = " ~ Assert: ~";
             Char2speech.text = "Ask for something specific";
         }
         else if (primeInt == 12) {
+            mainCharacter.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "YOU";
             Char2speech.text = "In the future, can you not say that?";
         }
         else if (primeInt == 13) {
+            mainCharacter.SetActive(false);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = " ~ Reinforce: ~";
             Char2speech.text = "Explain the good outcomes! Be kind!";
         }
         else if (primeInt == 14) {
+            mainCharacter.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "YOU";
             Char2speech.text = "It would make me feel really good if you’d be respectful to everyone. Thank you!";
         }
         else if (primeInt == 15) {
+            mainCharacter.SetActive(false);
+            ArtChar1.SetActive(true);
             Char1name.text = "CONSTANCE";
             Char1speech.text = "I’m sorry! Thank you for telling me all of that.";
             Char2name.text = "";
@@ -146,12 +164,14 @@ public void talking()
             Char2speech.text = "";
         }
         else if (primeInt == 17) {
+            ArtChar1.SetActive(false);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "";
             Char2speech.text = "We should probably reassure Constance her body is good just the way it is, and that fat is not a feeling!";
         }
         else if (primeInt == 18) {
+            mainCharacter.SetActive(true);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "YOU";
@@ -164,18 +184,24 @@ public void talking()
             Char2speech.text = "Not eating enough at lunch is way worse than eating a filling meal, you won’t have enough energy to focus in class or play in gym class!";
         }
         else if (primeInt == 20) {
+            mainCharacter.SetActive(false);
+            ArtChar1.SetActive(true);
             Char1name.text = "CONSTANCE";
             Char1speech.text = "You’re right, I should probably get a sandwich or burger too…";
             Char2name.text = "";
             Char2speech.text = "";
         }    
         else if (primeInt == 21) {
+            mainCharacter.SetActive(true);
+            ArtChar1.SetActive(false);
             Char1name.text = "";
             Char1speech.text = "";
             Char2name.text = "YOU";
             Char2speech.text = "Plus, fat isn’t a feeling, and bodies don’t need to be changed to be good. You are good just the way you are!";
         }
         else if (primeInt == 22) {
+            mainCharacter.SetActive(false);
+            ArtChar1.SetActive(true);
             Char1name.text = "CONSTANCE";
             Char1speech.text = "Thanks! That means a lot!";
             Char2name.text = "";
@@ -189,26 +215,10 @@ public void talking()
 
 // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
         public void Choice1aFunct(){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "You";
-                Char2speech.text = "I don't know what you're talking about!";
-                primeInt = 99;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
+                
         }
         public void Choice1bFunct(){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "You";
-                Char2speech.text = "Sure, anything you want... just lay off the club.";
-                primeInt = 199;
-                Choice1a.SetActive(false);
-                Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
+                
         }
 
         public void SceneChange1(){
